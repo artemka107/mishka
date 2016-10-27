22d19ef4e863b1c958c8ce668531c7cce38063df
artemka107 <artem-ivanec@mail.ru>
2016-10-28 01:21:58 +0300

    Update 2016-10-27T22:21:50.783Z

---
+++ js/minjs/burger.js
@@ -0,0 +1,1 @@
+var burger=document.querySelector(".burger");burger.addEventListener("click",function(){burger.classList.contains("burger--closed")?burger.classList.remove("burger--closed"):burger.classList.add("burger--closed")});
